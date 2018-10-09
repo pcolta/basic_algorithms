6d28c69998517df15ec4f39b5bdc0f3240404269
Patrycja <p.colta1@gmail.com>
2018-10-09 17:43:50 +0200

    first commit

--- first_request.py
+++
@@ -1,6 +0,0 @@
-import requests
-from PIL import Image
-from StringIO import StringIO
-
-r = requests.get('https://github.com/timeline.json')
-
--- test_zadania_do_testowania.py
+++
@@ -1,8 +0,0 @@
-import zadania_do_testowania
-import unittest
-
-class TestZadaniaDoTestowania(unittest.TestCase):
-
-    def testAddValue(self
-        """add_value should add a to b"""
-        result =
